4dc1181db9ef5419a8139e74588b4eec6ef86cd5
Jefferson <je_jmgbrasil@hotmail.com>
2019-05-27 11:29:51 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+ 
+app.use(bodyParser.json())
+
+var students = [{"nome": "Marcos", "idade": "23"},
+	{"nome": "Pedro", "idade": "27"},
+	{"nome": "Lucas", "idade": "20"}
+]
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/students', function (req, res) {
+  res.send(students);
+})
+
+app.post('/students', function (req, res) {
+  var student = req.body;
+  students.push(student);
+  res.send("Estudante cadastrado com sucesso");
+})
+
+app.delete('/students', function (req, res) {
+  students = [];
+  res.send("Todos os estudante foram removidos com sucesso");
+})
+
+app.get('/students/:name', function (req, res) {
+  let name = req.params.name;
+  let filteredstudent = students.filter ( (s) => {return (s.nome == name)} );
+  if(filteredstudent.length >= 1){
+    res.send(filteredstudent[0]);
+  }else res.status(404).send("Estudante não encontrado");
+  res.send(name);
+})
+
+
+app.post('/', function (req, res) {
+  res.sendStatus(429);
+})
+ 
+app.listen(3000)
