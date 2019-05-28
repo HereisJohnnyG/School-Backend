4f7f9a98aec7c91b536d649b51293dd15246c3b7
Jefferson <je_jmgbrasil@hotmail.com>
2019-05-28 11:46:02 -0300

    routes

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const baseAPI = "/api/v1";
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/api/v1', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post('/', function (req, res) {
+app.post('/api/v1', function (req, res) {
   res.send('Hello World');
 });
 
-app.use(`${baseAPI}/students`, require('./routes/students'));
+app.use(`${baseAPI}/students`, require('./routes/student'));
 app.use(`${baseAPI}/course`, require('./routes/course').app);
 app.use(`${baseAPI}/teacher`, require('./routes/teacher').app);
-app.use(`${baseAPI}/users`, require('./routes/users'));
+app.use(`${baseAPI}/users`, require('./routes/user'));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on PORT ${listener.address().port}`);
