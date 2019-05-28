52fc48af6722091c673ad7544bb0e8de6a3e1769
Jefferson <je_jmgbrasil@hotmail.com>
2019-05-28 12:18:04 -0300

    teacher id fixed

--- routes/teacher.js
+++ routes/teacher.js
@@ -29,7 +29,7 @@ app.get('/:id', function (req, res) {
 
 app.post('/', function (req, res) {
   var professor = req.body;
-  teacher['id'] = ++id;
+  professor['id'] = ++id;
   teacher.push(professor);
   res.status(201).send("Professor cadastrado com sucesso");
 })
