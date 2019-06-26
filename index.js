e87a0d6064f9be0b1c168af40375b40b2a9fc352
Jefferson <je_jmgbrasil@hotmail.com>
2019-06-26 17:55:30 -0300

    add Auth autentication

--- index.js
+++ index.js
@@ -3,11 +3,40 @@ const bodyParser = require('body-parser');
 const app = express();
 const conn = require('./config')
 const cors = require('cors');
+const jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
 conn();
 
-
 const baseAPI = "/api/v1";
+
+const baseAPI_Auth = "/api/v1.1";
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://dev-47b3881g.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'sadadad',
+  issuer: 'https://dev-47b3881g.auth0.com/',
+  algorithms: ['RS256']
+})
+
+app.use(jwtCheck);
+app.use( jwtCheck.unless({path: `${baseAPI}`}) );
+
+
+app.get('/authorized', function (req, res) {
+  res.send('Secured Resource');
+});
+
+app.get(`${baseAPI}/protected`,
+  function(req, res) {
+    if (!req.user.admin) return res.sendStatus(401);
+    res.sendStatus(200);
+});
 
 app.use(cors());
 app.options('*', cors());
@@ -31,6 +60,17 @@ app.use(`${baseAPI}/user`, require('./routes/user'));
 
 //------------------JSON-----------------------------------//
 app.use(`${baseAPI}/JSON`, require('./routes/json'));
+
+
+// ROUTE CONFIGURATION
+
+app.use(`${baseAPI_Auth}/student`, require('./routes/student'));
+app.use(`${baseAPI_Auth}/course`, require('./routes/course'));
+app.use(`${baseAPI_Auth}/teacher`, require('./routes/teacher'));
+app.use(`${baseAPI_Auth}/user`, require('./routes/user'));
+
+//------------------JSON-----------------------------------//
+app.use(`${baseAPI_Auth}/JSON`, require('./routes/json'));
 
 
 //Listening on Heroku or localhost:3000
